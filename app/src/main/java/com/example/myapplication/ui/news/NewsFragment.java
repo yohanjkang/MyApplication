6f552aa98cf9bb416c0e6583548b29552e6fed4e
Yohan Kang <yohankan@usc.edu>
2021-03-21 04:51:39 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/myapplication/ui/news/NewsFragment.java
@@ -0,0 +1,28 @@
+package com.example.myapplication.ui.news;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.R;
+
+public class NewsFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.fragment_news, container, false);
+        Intent intent = new Intent(getActivity(), NewsActivity.class);
+        startActivity(intent);
+        return root;
+    }
+}
